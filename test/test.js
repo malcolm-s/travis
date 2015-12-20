04c538e27806446497a1bc31b5a0df0ab1538ade
Malcolm Stone <malcolm.stone91@gmail.com>
2015-12-20 18:51:25 +0000

    Added more tests

--- test/test.js
+++ test/test.js
@@ -8,4 +8,25 @@ describe('Test', function() {
   it('should pass now', function() {
     assert.equal(2, 2);
   });
+
+  it('should handle promises', function() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(resolve, 500);
+    });
+  });
+
+  it('should take a short time', function(done) {
+    setTimeout(done, 500);
+  });
+
+  it('should take a long time', function(done) {
+    setTimeout(done, 1500);
+  });
+
+  if (true) {    
+    for (var i = 0; i < 1000; i++) {
+      it(`should generate a test at index ${i}`, function() {
+      });
+    }
+  }
 });
